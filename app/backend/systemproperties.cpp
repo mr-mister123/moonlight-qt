1309acb61f2c7a074b2539a2b39b451a924375ba
mr-mister123 <mr.mister123@hotmail.com>
2026-01-12 13:58:03 +0100

    set testframe-size to full-hd

--- app/backend/systemproperties.cpp
+++ app/backend/systemproperties.cpp
@@ -139,14 +139,14 @@ void SystemProperties::querySdlVideoInfoInternal()
     // We call the internal variant because we're already in a safe thread context.
     refreshDisplaysInternal();
 
-    SDL_Window* testWindow = SDL_CreateWindow("", 0, 0, 1280, 720,
+    SDL_Window* testWindow = SDL_CreateWindow("", 0, 0, 1920, 1080,
                                               SDL_WINDOW_HIDDEN | StreamUtils::getPlatformWindowFlags());
     if (!testWindow) {
         SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION,
                     "Failed to create test window with platform flags: %s",
                     SDL_GetError());
 
-        testWindow = SDL_CreateWindow("", 0, 0, 1280, 720, SDL_WINDOW_HIDDEN);
+        testWindow = SDL_CreateWindow("", 0, 0, 1920, 1080, SDL_WINDOW_HIDDEN);
         if (!testWindow) {
             SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
                          "Failed to create window for hardware decode test: %s",
